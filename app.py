c81d002fb8b3c53ea1bbb12b5e6a62bcc50a84de
ravitejakodati <45182753+ravitejakodati@users.noreply.github.com>
2018-11-27 17:03:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,27 @@ def preprocessor(text):
   text = (re.sub('[\W]+', ' ', text.lower()) + ' ' + ' '.join(emoticons).replace('-', ''))
   return text
 
+tweet_classifier = pickle.load(open('../data/logisticRegression.pkl', 'rb'))
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return app.send_static_file('html/index.html')
+ 
+@app.route('/classify', methods=['POST'])
+def classify():
+    text = request.form.get('text', None)
+    assert text is not None
+    prob_neg, prob_pos = tweet_classifier.predict_proba([text])[0]
+    s = 'Positive' if prob_pos >= prob_neg else 'Negative'
+    p = prob_pos if prob_pos >= prob_neg else prob_neg
+    return jsonify({
+        'sentiment': s,
+        'probability': p
+    })
+
+
+
+
     
    
